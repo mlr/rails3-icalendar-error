d437cb617ae0aaeccdb2ccfd2edc1254d12253c6
Ronnie Miller <r.miller@crowdcompass.com>
2015-06-29 13:01:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3IcalendarBug::Application.config.secret_token = '311c17b56d99b38f5b26665b6f2c6d31af59cb4d83a5c2c5d5b1c8a5ac6e2978726d2cf79ee94e6feef388d8d71cab50811c742fac07c990146e27a7bbc96427'
